610b35bff13a6e0388f491c58d06e0b2b11b8647
Siwadon Saosoong <siwadon.s@gmail.com>
2018-06-01 21:33:52 -0700

    #2 Allow CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
+import * as cors from 'cors';
 import * as functions from 'firebase-functions';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.twitter.com/1.1';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + functions.config().twitter.token;
 const DEFAULT_NUMBER_OF_TWEETS = 20;
+const corsHandler = cors({ origin: '*' });
 
 /**
  * Search tweets endpoint
@@ -11,23 +13,25 @@ const DEFAULT_NUMBER_OF_TWEETS = 20;
  * If success, it will return N tweets matching the query. Otherwise, responses with an appropriate error.
  */
 export const search = functions.https.onRequest((request, response) => {
-  if (request.query.q === undefined || request.query.q === '') {
-    response.status(400).send('Missing q parameter');
-    return;
-  }
-  const url = '/search/tweets.json';
-  const params = {
-    q: request.query.q,
-    count: request.query.count || DEFAULT_NUMBER_OF_TWEETS,
-  };
+  corsHandler(request, response, () => {
+    if (request.query.q === undefined || request.query.q === '') {
+      response.status(400).send('Missing q parameter');
+      return;
+    }
+    const url = '/search/tweets.json';
+    const params = {
+      q: request.query.q,
+      count: request.query.count || DEFAULT_NUMBER_OF_TWEETS,
+    };
 
-  axios
-    .get(url, { params })
-    .then(twitterResponse => {
-      response.json(twitterResponse.data);
-    })
-    .catch(error => {
-      console.log(error);
-      response.status(500).json({ status: 500, message: 'Something went wrong' });
-    });
+    axios
+      .get(url, { params })
+      .then(twitterResponse => {
+        response.json(twitterResponse.data);
+      })
+      .catch(error => {
+        console.log(error);
+        response.status(500).json({ status: 500, message: 'Something went wrong' });
+      });
+  });
 });
