39a686122ce00ed24783f2d5827ff057062b9055
Siwadon Saosoong <siwadon.s@gmail.com>
2018-06-04 13:39:45 -0700

    Correct the import statement for material-ui that supports tree shaking
    this reduces the bundle size by 22%

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-import { withStyles, Hidden } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import yellow from '@material-ui/core/colors/yellow';
